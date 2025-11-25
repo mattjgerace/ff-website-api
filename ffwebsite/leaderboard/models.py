ea8cf207f22199c38715c9470f5a1aee15a73815
Matt Gerace <matthewjgerace@gmail.com>
2025-11-25 17:03:41 -0500

    Initialize Models

--- ffwebsite/leaderboard/models.py
+++ ffwebsite/leaderboard/models.py
@@ -1,12 +1,9 @@
 from django.db.models import Model
 from django.db.models.fields import CharField,IntegerField, BooleanField, DecimalField
-from django.db.models import JSONField, ForeignKey, CASCADE, Sum
+from django.db.models import JSONField, ForeignKey, CASCADE, OneToOneField, ManyToManyField
 from django.contrib.postgres.fields import ArrayField
-from django_cryptography.fields import encrypt
 
-from django.db import connection, reset_queries #delete later
-
-class TeamOwnerAPP(Model):
+class TeamManagerAPP(Model):
     first_name = CharField(max_length=30)
     last_name = CharField(max_length=30)
     active = BooleanField()
@@ -15,72 +12,115 @@ class TeamOwnerAPP(Model):
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
 
-class TeamOwnerESPN(Model):
-    team_owner = ForeignKey(TeamOwnerAPP, on_delete=CASCADE)
-    espn_team_id = IntegerField()
+class TeamManagerESPN(Model):
+    team_manager = ForeignKey(TeamManagerAPP, on_delete=CASCADE)
+    team_id = CharField()
 
-class TeamOwnerSLEEPER(Model):
-    team_owner = ForeignKey(TeamOwnerAPP, on_delete=CASCADE)
-    sleeper_team_id = encrypt(CharField())
-    sleeper_roster_id = IntegerField()
+class TeamManagerSLEEPER(Model):
+    team_manager = ForeignKey(TeamManagerAPP, on_delete=CASCADE)
+    team_id = CharField()
+    user_id = CharField()
 
-#need to figure out how to keep track of team player is on
 class Player(Model):
-    first_name = CharField(max_length=30) #Player's First Name
-    last_name = CharField(max_length=30) #Player's Last Name
-    position = CharField(max_length=5) #Player's Position
-    sleeper_player_id = CharField(null=True) #Sleeper ID
-    espn_player_id = CharField(null=True) #ESPN ID
+    first_name = CharField(max_length=30)
+    last_name = CharField(max_length=30)
+    position = CharField(max_length=5)
 
-class Draft(Model): #Should this be results based mixin?
-    date = IntegerField(null=True)
-    draft_type = CharField(null=True)
-    draft_settings = JSONField(null=True) #can these be not null?
-    order = JSONField(null=True) #probably don't need
+class PlayerSLEEPER(Model):
+    player = ForeignKey(Player, on_delete=CASCADE)
+    external_player_id = CharField()
 
-class DraftPicks(Model):
-    draft = ForeignKey(Draft, on_delete=CASCADE) # Draft id
-    team = ForeignKey(TeamOwnerAPP, on_delete=CASCADE)
-    player = ForeignKey(Player, on_delete=CASCADE) # Player id
-    round_num = IntegerField()
-    pick_num = IntegerField()
+class PlayerESPN(Model):
+    player = ForeignKey(Player, on_delete=CASCADE)
+    external_player_id = CharField()
 
 class SeasonSettings(Model):
     season = IntegerField() #Specifies the year in which the season took place
-    platform = CharField() #Specifies which platform the season was played on 
+    platform = CharField() #Specifies which platform the season was played on
+    playoff_week_start = IntegerField() 
+    division_mapping = JSONField() #Specifies the division naming
     league_settings = JSONField() #Specifies the league's rules for the season
     scoring_settings = JSONField() #Specifies the league's scoring rules for the season
     roster_settings = ArrayField(CharField()) #Specifies the league's roster configuration for the season
-    draft = ForeignKey(Draft, on_delete=CASCADE, null=True) # Draft id
+
+    @property
+    def platform_season_settings(self):
+        match self.platform:
+            case "sleeper":
+                return self.seasonsettingssleeper_set.first()
+            case "espn":
+                return self.seasonsettingsespn_set.first()
+            
+class SeasonSettingsESPN(Model):
+    season_settings = ForeignKey(SeasonSettings, on_delete=CASCADE)
+    league_id = CharField()
+
 
 class SeasonSettingsSLEEPER(Model):
     season_settings = ForeignKey(SeasonSettings, on_delete=CASCADE)
-    sleeper_league_id = encrypt(CharField())
-    sleeper_draft_id = encrypt(CharField())
-    sleeper_bracket_id = encrypt(CharField())
+    league_id = CharField()
+    draft_id = CharField()
+    bracket_id = CharField()
+
+class Draft(Model):
+    season_settings = OneToOneField(SeasonSettings, on_delete=CASCADE)
+    date = IntegerField(null=True)
+    draft_type = CharField(null=True)
+    draft_settings = JSONField(null=True) #can these be not null?
+    order = JSONField(null=True) #might not need
+
+class Playoff(Model):
+    season_settings = OneToOneField(SeasonSettings, on_delete=CASCADE)
+    playoff_teams = IntegerField()
+    #reseed = BooleanField()
+
+    #Adding these now for now
+    #champion = ForeignKey(TeamManagerAPP, on_delete=CASCADE)
+    #second = ForeignKey(TeamManagerAPP, on_delete=CASCADE)
+    #third = ForeignKey(TeamManagerAPP, on_delete=CASCADE)
+    #fourth = ForeignKey(TeamManagerAPP, on_delete=CASCADE)
+
+class DraftPicks(Model):
+    draft = ForeignKey(Draft, on_delete=CASCADE)
+    team = ForeignKey(TeamManagerAPP, on_delete=CASCADE)
+    player = ForeignKey(Player, on_delete=CASCADE)
+    round_num = IntegerField()
+    pick_num = IntegerField()
 
 class WeeklyMatchups(Model):
-    season_settings = ForeignKey(SeasonSettings, on_delete=CASCADE) # Season the game took place
-    week = IntegerField() # Week the game took place
-    team = ForeignKey(TeamOwnerAPP, on_delete=CASCADE) # team id
-    opp = ForeignKey(TeamOwnerAPP, on_delete=CASCADE, related_name='opponent') # opp team id #this could be repetitive ...
+    season_settings = ForeignKey(SeasonSettings, on_delete=CASCADE)
+    week = IntegerField()
+    team = ForeignKey(TeamManagerAPP, on_delete=CASCADE)
+    opp = ForeignKey(TeamManagerAPP, on_delete=CASCADE, related_name='opponent') #opp team id--this could be repetitive ...
     score = DecimalField(max_digits=5, decimal_places=2, null=True)
+    result = CharField()
+    weekly_winner = BooleanField(default=False)
+    playoff = BooleanField(default=False)
+    
     #matchup = IntegerField() # ID representing the matchup
     #roster = ArrayField(CharField()) # Player id's #don't don't need
     #starters = ArrayField(CharField()) # Player id's#probably don't need
 
-class WeeklyWinner(Model):
-    weeklymatchup = ForeignKey(WeeklyMatchups, on_delete=CASCADE)
-
 class PlayerPoints(Model):
-    weeklymatchup = ForeignKey(WeeklyMatchups, on_delete=CASCADE) # Game information
-    player = ForeignKey(Player, on_delete=CASCADE) # Player id
-    points = DecimalField(max_digits=5, decimal_places=2) #Player Points
-    starter = BooleanField() #Determines if Player Started
+    weeklymatchup = ForeignKey(WeeklyMatchups, null=True, on_delete=CASCADE)
+    player = ForeignKey(Player, on_delete=CASCADE)
+    points = DecimalField(max_digits=5, decimal_places=2)
+    starter = BooleanField()
+    week = IntegerField()
+    season = IntegerField()
+    nfl_team = CharField(null=True)
+    injury_status = CharField(null=True)
 
 class Leaderboard(Model):
-    team = ForeignKey(TeamOwnerAPP, on_delete=CASCADE) # Team id
+    team = ForeignKey(TeamManagerAPP, on_delete=CASCADE)
     season_settings = ForeignKey(SeasonSettings, on_delete=CASCADE)
-
-class YearlyWinner(Model):
-    leaderboard = ForeignKey(Leaderboard, on_delete=CASCADE)
+    # wins = IntegerField()
+    # losses = IntegerField()
+    # ties = IntegerField()
+    standing = IntegerField(null=True)
+    division = CharField(null=True)
+    division_standing = IntegerField(null=True)
+    seed = IntegerField(null=True)
+    draft_pick = IntegerField(null=True)
+    season_winner = BooleanField(default=False)
+    division_winner = BooleanField(default=False)
