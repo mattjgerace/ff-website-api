e5d94406033270c9f40414b4491c327dfdff5fdd
Matt Gerace <matthewjgerace@gmail.com>
2025-11-25 20:16:12 -0500

    adjust settings

--- ffwebsite/ffwebsite/settings.py
+++ ffwebsite/ffwebsite/settings.py
@@ -128,17 +128,18 @@ WSGI_APPLICATION = 'ffwebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ["DB_NAME"], 
-        'USER': os.environ["DB_USER"], 
-        'PASSWORD': os.environ["DB_PASSWORD"],
-        'HOST': os.environ["DB_HOST"], 
-        'PORT': os.environ["DB_PORT"],
-        'DATABASE_URL': os.environ["DATABASE_URL"]
+if os.env["ENVIRONMENT"] != "ghactions":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ["DB_NAME"], 
+            'USER': os.environ["DB_USER"], 
+            'PASSWORD': os.environ["DB_PASSWORD"],
+            'HOST': os.environ["DB_HOST"], 
+            'PORT': os.environ["DB_PORT"],
+            'DATABASE_URL': os.environ["DATABASE_URL"]
+        }
     }
-}
 
 
 # Password validation
