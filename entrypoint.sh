fceaa88b8aac5d3d73f487ddbc385bae990ce24e
Matt Gerace <matthewjgerace@gmail.com>
2025-11-28 18:04:45 -0500

    update gunicorn port

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ echo "Applying database migrations..."
 python manage.py migrate --noinput
 
 echo "Starting Gunicorn..."
-gunicorn ffwebsite.wsgi:application --bind 0.0.0.0:8000
+gunicorn ffwebsite.wsgi:application
