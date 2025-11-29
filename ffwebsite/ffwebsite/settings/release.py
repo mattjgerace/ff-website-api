ddb90bfb75f202039a4243b4a84c8ca320505b93
Matt Gerace <matthewjgerace@gmail.com>
2025-11-28 19:09:57 -0500

    update release.py

--- ffwebsite/ffwebsite/settings/release.py
+++ ffwebsite/ffwebsite/settings/release.py
@@ -4,16 +4,22 @@ from .base import *
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.environ["DB_NAME"], 
+#         'USER': os.environ["DB_USER"], 
+#         'PASSWORD': os.environ["DB_PASSWORD"],
+#         'HOST': os.environ["DB_HOST"], 
+#         'PORT': os.environ["DB_PORT"],
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ["DB_NAME"], 
-        'USER': os.environ["DB_USER"], 
-        'PASSWORD': os.environ["DB_PASSWORD"],
-        'HOST': os.environ["DB_HOST"], 
-        'PORT': os.environ["DB_PORT"],
-    }
+    "default": dj_database_url.parse(os.environ["DATABASE_URL"])
 }
+
+DEBUG = False
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
