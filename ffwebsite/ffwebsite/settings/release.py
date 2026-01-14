c90025ef0322b36b9639157363ab64b5ff69dcda
mattjgerace <matthewjgerace@gmail.com>
2026-01-14 14:20:57 -0500

    prepare for release

--- ffwebsite/ffwebsite/settings/release.py
+++ ffwebsite/ffwebsite/settings/release.py
@@ -5,20 +5,20 @@ from .base import *
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.environ["DB_NAME"], 
-#         'USER': os.environ["DB_USER"], 
-#         'PASSWORD': os.environ["DB_PASSWORD"],
-#         'HOST': os.environ["DB_HOST"], 
-#         'PORT': os.environ["DB_PORT"],
-#     }
-# }
-
 DATABASES = {
-    "default": dj_database_url.parse(os.environ["DATABASE_URL"])
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ["DB_NAME"],
+        'USER': os.environ["DB_USER"],
+        'PASSWORD': os.environ["DB_PASSWORD"],
+        'HOST': os.environ["DB_HOST"],
+        'PORT': os.environ["DB_PORT"],
+    }
 }
+
+# DATABASES = {
+#     "default": dj_database_url.parse(os.environ["DATABASE_URL"])
+# }
 
 MONGO_CLIENT = MongoClient(os.environ["MONGO_URL"], 
                            maxIdleTimeMS=30000,
